61eb22322849d0798749f0fd25f16ccc0c21c708
SidAdi <37907559+SidAdi@users.noreply.github.com>
2018-06-03 19:43:51 +0530

    Update factorial.java

--- JavaPractice/src/factorial.java
+++ JavaPractice/src/factorial.java
@@ -4,6 +4,7 @@ public class factorial
 	public static void main(String args[])
 	{
 		System.out.println("new code changes");
+		System.out.println("New requirement");
 		int i;
 		int f=1;
 		int n=5;
